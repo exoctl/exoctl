8db158cfd2e2753f4becc8120fb60c46b8d91d18
VitorMob <mobhacking100@gmail.com>
2024-08-31 22:34:00 -0300

    feat: adjust members in class for initialize in constructor class

--- sources/engine/data/magic.cxx
+++ sources/engine/data/magic.cxx
@@ -1,25 +1,23 @@
-#include <engine/data/magic.hxx>
 #include <engine/data/data_exception.hxx>
+#include <engine/data/magic.hxx>
 
 namespace Data
 {
 Magic::Magic() : m_cookie(magic_open(MAGIC_MIME))
 {
     if (m_cookie == nullptr)
-    {
-        throw DataException::Initialize("magic_open() failed to return a cookie");
-    }
+        throw DataException::Initialize(
+            "magic_open() failed to return a cookie");
 
     if (magic_load(m_cookie, nullptr) != 0)
-    {
-        throw DataException::Initialize("magic_load failed to load magic database");
-    }
+        throw DataException::Initialize(
+            "magic_load failed to load magic database");
 }
 Magic::~Magic() { magic_close(m_cookie); }
 
-const void Magic::magic_load_mime(const std::string &buffer)
+const void Magic::magic_load_mime(const std::string &p_buffer)
 {
-    m_mime = magic_buffer(m_cookie, buffer.c_str(), buffer.size());
+    m_mime = magic_buffer(m_cookie, p_buffer.c_str(), p_buffer.size());
 }
 
 const std::string Magic::magic_get_mime() { return m_mime; }
--- sources/engine/data/metadata.cxx
+++ sources/engine/data/metadata.cxx
@@ -1,56 +1,52 @@
-#include <engine/data/metadata.hxx>
 #include <cmath>
+#include <engine/data/metadata.hxx>
 
 namespace Data
 {
-Metadata::Metadata()
+Metadata::Metadata() : m_current_time(time(0)), m_entropy(0.0)
 {
-    m_entropy = 0.0;
-    m_current_time = time(0);
-
     dto_set_field("mime_type", "none");
     dto_set_field("sha256", "none");
     dto_set_field("size", 0);
     dto_set_field("creation_date", "none");
-    dto_set_field("entropy", 0.0);
+    dto_set_field("entropy", m_entropy);
 }
 
 Metadata::~Metadata() {}
 
-const void Metadata::metadata_parse(const std::string &buffer)
+const void Metadata::metadata_parse(const std::string &p_buffer)
 {
-    m_magic.magic_load_mime(buffer);
+    m_magic.magic_load_mime(p_buffer);
     dto_set_field("mime_type", m_magic.magic_get_mime());
 
-    m_sha.sha_gen_sha256_hash(buffer);
+    m_sha.sha_gen_sha256_hash(p_buffer);
     dto_set_field("sha256", m_sha.sha_get_sha256_hash());
 
-    dto_set_field("size", (int) buffer.size());
+    dto_set_field("size", (int) p_buffer.size());
 
     tm *ltm = localtime(&m_current_time);
     char cstr[11];
     strftime(cstr, sizeof(cstr), "%Y-%m-%d", ltm);
 
-    std::string current_time = cstr;
-    dto_set_field("creation_date", current_time);
+    dto_set_field("creation_date", std::string(cstr));
 
-    compute_entropy(buffer);
+    compute_entropy(p_buffer);
     dto_set_field("entropy", m_entropy);
 }
 
-const void Metadata::compute_entropy(const std::string &buffer)
+const void Metadata::compute_entropy(const std::string &p_buffer)
 {
     size_t map[256] = {0};
 
-    for(size_t i = 0; i < buffer.size(); i++)  
-        map[static_cast<unsigned char>(buffer[i])]++;
+    for (size_t i = 0; i < p_buffer.size(); i++)
+        map[static_cast<unsigned char>(p_buffer[i])]++;
 
-    double recip = 1.0 / buffer.size();
+    double recip = 1.0 / p_buffer.size();
     double entropy = 0.0;
 
-    for(size_t i = 0; i < 256; i++)
-    { 
-        if(map[i])
+    for (size_t i = 0; i < 256; i++)
+    {
+        if (map[i])
         {
             double freq = map[i] * recip;
             entropy += freq * log2(freq);
