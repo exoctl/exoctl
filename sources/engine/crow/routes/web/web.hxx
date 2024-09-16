dba506bbb7478782fe06785ce62e7609dc070d01
VitorMob <mobhacking100@gmail.com>
2024-09-16 13:06:16 -0300

    feat: add parameters in endpoint web

--- sources/engine/crow/routes/web/web.hxx
+++ sources/engine/crow/routes/web/web.hxx
@@ -7,14 +7,26 @@
 
 namespace Crow
 {
-class Web
+template <typename... Args> class Web
 {
   public:
     using on_request_callback =
-        std::function<crow::response(const crow::request &)>;
+        std::function<crow::response(const crow::request &, Args...)>;
 
-    Web(CrowApp &, const std::string &, on_request_callback);
-    ~Web();
+    Web(CrowApp &p_crow,
+        const std::string &p_url,
+        on_request_callback on_request)
+        : m_crow(p_crow), m_url(p_url), m_on_request(on_request)
+    {
+        LOG(m_crow.crow_get_log(),
+            info,
+            "Creating HTTP route for URL: '{}'",
+            m_url);
+
+        m_crow.crow_get_app().route_dynamic(m_url)(m_on_request);
+    }
+
+    ~Web() {}
 
   private:
     CrowApp &m_crow;
