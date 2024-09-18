31774150b26389ba3c2063364cc4288684c92471
VitorMob <mobhacking100@gmail.com>
2024-09-17 21:06:02 -0300

    feat: no format clang

--- sources/log.hxx
+++ sources/log.hxx
@@ -2,6 +2,7 @@
 
 #include <fmt/core.h>
 
+// clang-format off
 #define LOG_INFO(...) fmt::print("[INFO] {}\n", fmt::format(__VA_ARGS__))
-#define LOG_ERROR(...)                                                         \
-    fmt::print(stderr, "[ERROR] {}\n", fmt::format(__VA_ARGS__))
+#define LOG_ERROR(...) fmt::print(stderr, "[ERROR] {}\n", fmt::format(__VA_ARGS__))
+// clang-format on
