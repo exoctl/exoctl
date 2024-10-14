c4953ffd2a84a2cc944ef69cf34d4de353371dbb
VitorMob <mobhacking100@gmail.com>
2024-10-14 00:12:57 -0300

    feat: add color in habbit

--- sources/main.cxx
+++ sources/main.cxx
@@ -2,9 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-    fmt::print("     (\\(\\             (\\__/)\n"
-               "     ( -.-) - yes...  (o.o ) - engine fast as a bunny!\n"
-               "     o_(\")(\")         (\")(\")\n");
+    fmt::print(
+        "\033[35m     (\\(\\            \033[0m\033[37m(\\__/)\n"
+        "\033[35m     ( -.-) - \033[36myes...\033[0m \033[37m(o.o )\033[32m - engine fast as a bunny!\n" // Texto em ciano e partes do coelho em branco
+        "\033[35m     o_(\")(\")        \033[0m\033[37m(\")(\")\n\033[0m" 
+    );
 
     program::Program program(argc, (const char **) argv);
     return program.run();
