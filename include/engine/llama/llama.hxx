0a4545e4b057c671dfe83feb2d9a1db0650636b6
VitorMob <mobhacking100@gmail.com>
2024-12-09 16:10:10 -0300

    feat: create simple functions for load context and model llama

--- include/engine/llama/llama.hxx
+++ include/engine/llama/llama.hxx
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <llama.h>
+#include <string>
 
-class Llama 
+class Llama
 {
+  public:
+    Llama();
+    ~Llama() = default;
 
+    bool load_model(const std::string &, llama_model_params);
+    bool load_context(llama_context_params);
+
+  private:
+    llama_context *m_context;
+    llama_model *m_model;
 };
--- sources/engine/llama/llama.cxx
+++ sources/engine/llama/llama.cxx
@@ -1,2 +1,25 @@
 #include <engine/llama/llama.hxx>
 
+Llama::Llama()
+{
+}
+
+bool Llama::load_model(const std::string &path, llama_model_params p_params)
+{
+    m_model = llama_load_model_from_file(path.c_str(), p_params);
+
+    if (!m_model)
+        return false;
+
+    return true;
+}
+
+bool Llama::load_context(llama_context_params p_params)
+{
+    m_context = llama_new_context_with_model(m_model, p_params);
+
+    if (!m_context)
+        return false;
+
+    return true;
+}
