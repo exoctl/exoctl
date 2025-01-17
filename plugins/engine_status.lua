26ce4763abde332c8270e90fec59ea186fb469f2
VitorMob <mobhacking100@gmail.com>
2025-01-17 01:05:13 -0300

    feat: create status engine script

---
+++ plugins/engine_status.lua
@@ -0,0 +1,3 @@
+-- verify status engine
+local is_running = tostring(engine.is_running())
+print("Engine status running: '" .. is_running .. "'")
--- plugins/webhook.lua
+++
@@ -1,2 +0,0 @@
-print("[script-webhook] running engine = " .. tostring(engine.is_running()))
-print("[script-webhook] running engine = " .. tostring(engine.get_bindaddr()))
