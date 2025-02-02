10e5abbd0cc9557f3871ee9adb028f296c26dc0c
VitorMob <mobhacking100@gmail.com>
2025-02-02 14:28:01 -0300

    feat: new docs (gitbook)

--- plugins/_example.lua
+++ plugins/_example.lua
@@ -1,18 +1,13 @@
--- Acessando uma propriedade (como 'is_running')
 logging:info(tostring(engine.is_running))
-
 
 local yara_instance = Yara.new()
 
--- Carregar regras do diretório
 yara_instance:load_rules(function ()
     yara_instance:load_rules_folder("rules/")    
 end)
 
-
 print("Rules loaded: ", yara_instance:get_rules_loaded_count())
 
--- Fazer um scan rápido de um buffer
 yara_instance:scan_fast_bytes("some_binary_data", function(status, rule, ns)
     if status == 1 then
         print("Match encontrado:", rule, "namespace:", ns)
